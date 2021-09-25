cca5794b3140f2489ecfec26fe550946515285f2
Slobodan Milutinovic <slobodan.milutinovic7@gmail.com>
2021-09-25 21:12:20 +0200

    Added tpl.execute and html template

--- main.go
+++ main.go
@@ -1,1 +1,26 @@
+package main
 
+import (
+	"html/template"
+	"net/http"
+	"os"
+)
+
+var tpl = template.Must(template.ParseFiles("index.html"))
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+
+	//w.Write([]byte("<h1>Hello World!</h1>"))
+	tpl.Execute(w, nil)
+}
+
+func main() {
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3005"
+	}
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", indexHandler)
+	http.ListenAndServe(":"+port, mux)
+}
