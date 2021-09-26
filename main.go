a1e3bcb3f133c7624530cd30bba7596dd5ff5a4b
Slobodan Milutinovic <slobodan.milutinovic7@gmail.com>
2021-09-26 20:15:05 +0200

    Added search handler function for search bar

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
+	"log"
 	"net/http"
+	"net/url"
 	"os"
+
+	"github.com/joho/godotenv"
 )
 
 var tpl = template.Must(template.ParseFiles("index.html"))
@@ -14,7 +19,29 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 	tpl.Execute(w, nil)
 }
 
+func searchHandler(w http.ResponseWriter, r *http.Request) {
+
+	u, err := url.Parse(r.URL.String())
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	params := u.Query()
+	searchQuery := params.Get("q")
+	page := params.Get("page")
+	if page == "" {
+		page = "1"
+	}
+	fmt.Println("Search query is:", searchQuery)
+	fmt.Println("Page is:", page)
+}
+
 func main() {
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("Error loading .env file")
+	}
 
 	port := os.Getenv("PORT")
 	if port == "" {
@@ -24,6 +51,7 @@ func main() {
 
 	mux := http.NewServeMux()
 	mux.Handle("/assets/", http.StripPrefix("/assets/", fs))
+	mux.HandleFunc("/search", searchHandler)
 	mux.HandleFunc("/", indexHandler)
 	http.ListenAndServe(":"+port, mux)
 }
