32f24d2373bdc864ecfe0dcdb7f3d0c3ae87ad62
Slobodan Milutinovic <slobodan.milutinovic7@gmail.com>
2021-09-27 12:26:56 +0200

    Added FetchEverything() function fetching results from typed query

--- main.go
+++ main.go
@@ -7,8 +7,11 @@ import (
 	"net/http"
 	"net/url"
 	"os"
+	"time"
 
 	"github.com/joho/godotenv"
+
+	"github.com/freshman-tech/news-demo-starter-files/news"
 )
 
 var tpl = template.Must(template.ParseFiles("index.html"))
@@ -19,39 +22,60 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 	tpl.Execute(w, nil)
 }
 
-func searchHandler(w http.ResponseWriter, r *http.Request) {
+func searchHandler(newsapi *news.Client) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		u, err := url.Parse(r.URL.String())
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
 
-	u, err := url.Parse(r.URL.String())
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
+		params := u.Query()
+		searchQuery := params.Get("q")
+		page := params.Get("page")
+		if page == "" {
+			page = "1"
+		}
+		fmt.Println("Search query is:", searchQuery)
+		fmt.Println("Page is:", page)
 
-	params := u.Query()
-	searchQuery := params.Get("q")
-	page := params.Get("page")
-	if page == "" {
-		page = "1"
+		results, err := newsapi.FetchEverything(searchQuery, page)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		fmt.Printf("%+v", results)
 	}
-	fmt.Println("Search query is:", searchQuery)
-	fmt.Println("Page is:", page)
 }
+
+var newsapi *news.Client
 
 func main() {
 
-	err := godotenv.Load()
+	err := godotenv.Load(".env")
 	if err != nil {
 		log.Println("Error loading .env file")
 	}
 
 	port := os.Getenv("PORT")
 	if port == "" {
+		fmt.Println("Port is empty")
 		port = "3005"
 	}
+
+	/*apiKey := os.Getenv("NEWS_API_KEY")
+	if apiKey == "" {
+		log.Fatal("API key is empty")
+	}*/
+
+	myClient := &http.Client{Timeout: 10 * time.Second}
+	newsapi := news.NewClient(myClient, "6c718b169bf1469c93829e1f26662b26", 20)
+
 	fs := http.FileServer(http.Dir("assets"))
 
 	mux := http.NewServeMux()
 	mux.Handle("/assets/", http.StripPrefix("/assets/", fs))
-	mux.HandleFunc("/search", searchHandler)
+	mux.HandleFunc("/search", searchHandler(newsapi))
 	mux.HandleFunc("/", indexHandler)
 	http.ListenAndServe(":"+port, mux)
 }
